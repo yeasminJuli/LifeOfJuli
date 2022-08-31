21137d8eef100329678f754e6a5cd763940b0b4d
kaziNAHIAN <provat44.knhp@gmail.com>
2022-08-31 12:16:23 +0600

    Revert "new user added"
    This reverts commit ebb011550c9fb90d5263824478785c28dce77397.

--- users/views.py
+++ users/views.py
@@ -6,7 +6,6 @@ def register(request):
     if request.method =='POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('home')
